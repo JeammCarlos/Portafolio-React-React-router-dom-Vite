4b20e5a3275028586498375f00e082105add9583
JeammCarlos <jeamcr.murillo@hotmail.com>
2024-04-29 13:03:52 -0600

    Se importo el Gyphi

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { useState, useEffect } from 'react'
 import { BrowserRouter } from "react-router-dom"
 import RoutesIndex from "../src/Routes/RoutesIndex"
 import Navbar from "../src/components/Navbar/Navbar"
-
+import Giphy from "../src/pages/Giphy"
 
 
 function App() {
