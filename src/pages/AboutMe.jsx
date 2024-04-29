a5e60be4fbaae5a2a97763202bbfbd48719064a9
JeammCarlos <jeamcr.murillo@hotmail.com>
2024-04-29 17:35:03 -0600

    Ordenamiento Code

--- src/pages/AboutMe.jsx
+++ src/pages/AboutMe.jsx
@@ -45,20 +45,6 @@ function InfoPersonal() {
                     </ul>
                 </section>
 
-                <section className="skills">
-                    <h2>Habilidades</h2>
-                    <ul>
-                        <li>Dominio del paquete Microsoft Office 365</li>
-                        <li>Dominio de HTML5 y CSS</li>
-                        <li>Dominio básico de JavaScript</li>
-                        <li>Excelentes habilidades de comunicación y colaboración</li>
-                        <li>Excelente conocimiento de los SGC bajo la norma ISO 9001:2015</li>
-                        <li>Excelente conocimiento de la NOM-059-SSA1-2015</li>
-                        <li>Excelentes habilidades para el uso de instrumentación dentro del laboratorio</li>
-                        <li>Excelentes habilidades para métodos analíticos: Cromatografía (HPLC; CG), espectrometría (EOS), espectroscopía UV-Vis, etc.</li>
-                    </ul>
-                </section>
-
                 <section className="education">
                     <h2>Educación</h2>
                     <ul>
--- src/pages/Skills.jsx
+++ src/pages/Skills.jsx
@@ -1,10 +1,21 @@
 import React from "react";
+import "../stylesheets/Skill.css";
 
 function InfoAdicional() {
     return(
-        <section>
-            <h2>Certificaciones</h2>
-        </section>
+        <section className="Skills">
+        <h2>Habilidades</h2>
+        <ul>
+            <li>Dominio del paquete Microsoft Office 365</li>
+            <li>Dominio de HTML5 y CSS</li>
+            <li>Dominio básico de JavaScript</li>
+            <li>Excelentes habilidades de comunicación y colaboración</li>
+            <li>Excelente conocimiento de los SGC bajo la norma ISO 9001:2015</li>
+            <li>Excelente conocimiento de la NOM-059-SSA1-2015</li>
+            <li>Excelentes habilidades para el uso de instrumentación dentro del laboratorio</li>
+            <li>Excelentes habilidades para métodos analíticos: Cromatografía (HPLC; CG), espectrometría (EOS), espectroscopía UV-Vis, etc.</li>
+        </ul>
+    </section>
     )
 }
 
